72c41ee76bd9267cae026bbe962fdfcd0126169f
Joaquín Chacón Groes-Petersen <jchacongp3@outlook.com>
2024-05-11 01:16:17 -0500

    nuevos cambios

--- BookSwap/Startup.cs
+++ BookSwap/Startup.cs
@@ -1,21 +1,5 @@
 ﻿
 
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using BookSwap.Data;
-using BookSwap.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 
 
 namespace BookSwap
